c179c950d9565e9e9f789744d7d33911bc9abd5b
Tower He <towerhe@gmail.com>
2011-07-23 17:35:40 +0800

    update to rspec 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'gmusic'
-require 'spec'
-require 'spec/autorun'
 
-Spec::Runner.configure do |config|
-  
+require 'rspec'
+require 'gmusic'
+
+RSpec.configure do |config|
+  config.mock_with :rspec
 end
 
 def path_of(file)
