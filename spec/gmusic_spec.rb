db79c3e1ea4946f3b955ff28cc470a6bff8de658
Jeweller-Tsai <jiangnan34@gmail.com>
2012-02-12 16:56:05 +0800

    eliminate useless comments

--- spec/gmusic_spec.rb
+++ spec/gmusic_spec.rb
@@ -1,20 +1,2 @@
 describe Gmusic do
-  # describe '.search' do
-  #   let(:query) { { song: 'Bad Romance', singer: 'Lady Gaga'} }
-  #   subject { Musicg.search(query) }
-
-  #   it 'returns a result object' do
-  #     should be_a Musicg::Result
-  #   end
-
-  #   context "there's no item matched" do
-  #     it 'contains corresponding result info' do
-
-  #     end
-  #   end
-
-  #   context "there're items matched" do
-  #     it 'contains corresponding result info'
-  #   end
-  # end
 end
