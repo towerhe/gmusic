c652c7d96f269d93a61867aa12904f62f3d34005
Jeweller-Tsai <jiangnan34@gmail.com>
2012-02-14 21:34:38 +0800

    use autoload to require files

--- lib/gmusic.rb
+++ lib/gmusic.rb
@@ -1,9 +1,7 @@
-require 'gmusic/search/agent'
-require 'gmusic/search/errors'
-
 module Gmusic
 
   module Search
+    autoload :Agent, 'gmusic/search/agent'
     SEARCH_URL = %Q{http://www.google.cn/music/search?q=}
     DOWNLOAD_URL = %Q(http://www.google.cn/music/top100/musicdownload?id=%s)
     SEARCH_OPTSTIONS = [:title, :artist, :album, :lyric]
