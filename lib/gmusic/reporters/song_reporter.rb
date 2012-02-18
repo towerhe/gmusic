8f738406970677c07192b7f207f099b6c397301d
Jeweller-Tsai <jiangnan34@gmail.com>
2012-02-18 19:44:17 +0800

    wrap search results in a table

--- lib/gmusic/reporters/song_reporter.rb
+++ lib/gmusic/reporters/song_reporter.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require "command_line_reporter"
 
 module Gmusic
@@ -7,11 +8,30 @@ module Gmusic
     alias old_header header
 
     def inilitialize(formatter = :progress)
-      self.formatter = formatter
+      self.formatter = formatter.to_s
     end
 
     def header(title)
-      old_header(title: title, color: 'green', width: 80, alain: 'center', rule: true, blod: true)
+      old_header(title: title, color: 'green', align: 'center', rule: true, bold: true)
+    end
+
+    def decorate(array)
+      header('搜索中...')
+      table border: true do
+        row color: 'red', bold: true do
+          column '歌名', width: 20, padding: 2
+          column '歌手', width: 20, padding: 2
+          column '链接', width: 45, padding: 2
+        end
+
+        array.each do |song|
+          row color: 'green' do
+            column song.title
+            column song.artist
+            column song.link
+          end
+        end
+      end
     end
   end
 end
