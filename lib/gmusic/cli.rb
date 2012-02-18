0a6ce349fd6607334239976cf7dfcfd2d90803bf
Jeweller-Tsai <jiangnan34@gmail.com>
2012-02-18 17:48:46 +0800

    provide command

--- lib/gmusic/cli.rb
+++ lib/gmusic/cli.rb
@@ -3,12 +3,20 @@ require 'gmusic'
 
 module Gmusic
   class CLI < Thor
-    map '-s' => :search
-
-    desc 'search', 'Search songs or albums'
-    method_options %w( title -t ) => :string, %w( album -a ) => :string
-    def search
-      puts SearchAgent.search(options).inspect
+    argument :option
+    desc 'song', 'Search and download songs'
+    method_options %w( title -t ) => :string
+    def song
+      case option
+      when 'search'
+        results = Song.search_by_title(options[:title].gsub(/-/, ' '))
+        reporter = SongReporter.new
+        reporter.decorate results
+      when 'download'
+        puts 'download'
+      else
+        raise 'invalid action'
+      end
     end
   end
 end
