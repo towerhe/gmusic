7f7e67eb6f10536969f608727fe9817ed1be3e34
Jeweller-Tsai <jiangnan34@gmail.com>
2012-02-18 19:47:17 +0800

    test

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -6,3 +6,8 @@ end
 那么 /^输出应该包括:$/ do |expected|
   assert_partial_output(expected, all_output)
 end
+
+那么 /^输出应该匹配\/([^\/]*)\/$/ do |expected|
+  assert_matching_output(expected, all_output)
+end
+
