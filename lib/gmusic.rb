f22913cabaf0b8d4a8682320ab89e4215f79cf77
Jeweller-Tsai <jiangnan34@gmail.com>
2012-02-18 17:42:23 +0800

    auto load required files

--- lib/gmusic.rb
+++ lib/gmusic.rb
@@ -1,7 +1,13 @@
 module Gmusic
+  autoload :Song, 'gmusic/models/song'
+  autoload :Artist, 'gmusic/models/artist'
+  autoload :Album, 'gmusic/models/album'
+  autoload :Link, 'gmusic/models/link'
+  autoload :SongReporter, 'gmusic/reporters/song_reporter'
 
   module Search
     autoload :Agent, 'gmusic/search/agent'
+
     SEARCH_URL = %Q{http://www.google.cn/music/search?q=}
     DOWNLOAD_URL = %Q(http://www.google.cn/music/top100/musicdownload?id=%s)
     SEARCH_OPTSTIONS = [:title, :artist, :album, :lyric]
