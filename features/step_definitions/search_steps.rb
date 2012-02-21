17161db2c8950dcc19dc94b4f92c35aed9311a85
Jeweller-Tsai <jiangnan34@gmail.com>
2012-02-21 20:57:45 +0800

    add a download feature

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -11,3 +11,10 @@ end
   assert_matching_output(expected, all_output)
 end
 
+那么 /^名为"([^"]*)"文件夹应该存在$/ do |directory|
+  check_directory_presence([directory], true)
+end
+
+那么 /^名为"([^"]*)"的文件应该存在$/ do |file|
+  check_file_presence([file], true)
+end
