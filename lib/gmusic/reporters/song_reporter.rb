8e2fd3d095d728638dc3ff9f5c888531ad648641
Jeweller-Tsai <jiangnan34@gmail.com>
2012-04-25 16:09:40 +0800

    Refactored

--- lib/gmusic/reporters/song_reporter.rb
+++ lib/gmusic/reporters/song_reporter.rb
@@ -5,33 +5,43 @@ module Gmusic
   class SongReporter
     include CommandLineReporter
 
-    alias old_header header
+    attr_accessor :subject
 
     def inilitialize(formatter = :progress)
       self.formatter = formatter.to_s
     end
 
     def header(title)
-      old_header(title: title, color: 'green',
-                 align: 'center', rule: true,
-                 bold: true)
+      super(title: title, color: 'green',
+            align: 'center', rule: true,
+            bold: true)
     end
 
-    def decorate(array)
-      header('搜索中...')
+    def list(array)
       table border: true do
+        thead = { id: 10, title: 20, artist: 20, url: 45 }
+
         row color: 'red', bold: true do
-          {'歌名' => 20, '歌手' => 20, '链接' => 45}.each { |k, v|
-            column k, width: v, padding: 2
-          }
+          thead.each { |k, v| column k, width: v, padding: 2 }
         end
 
-        array.each do |song|
+        array.each_with_index do |item, index|
           row color: 'green' do
-            [:title, :artist, :link].each { |method| column song.send(method) }
+            column (index + 1).to_s
+            thead.shift
+            thead.each_key { |method| column item.send(method) }
           end
         end
       end
     end
+
+    def self.decorate(&block)
+      new.tap do |reporter|
+        reporter.header '下载中'
+        reporter.subject = block.call
+        reporter.list reporter.subject
+      end
+    end
+
   end
 end
