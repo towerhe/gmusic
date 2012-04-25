768ba51895de77418d6e5836d16f519b1da59920
Jeweller-Tsai <jiangnan34@gmail.com>
2012-04-25 16:04:37 +0800

    Renamed link to url

--- lib/gmusic/models/song.rb
+++ lib/gmusic/models/song.rb
@@ -2,14 +2,14 @@ module Gmusic
   class InvalidAttributesError < StandardError; end
 
   class Song
-    attr_reader :title, :artist, :link
+    attr_reader :title, :artist, :url
 
     def initialize(attrs = {})
-      raise(InvalidAttributesError, 'only title, artist and link is allowed') if attrs_invalid?(attrs)
+      raise(InvalidAttributesError, 'only title, artist and url is allowed') if attrs_invalid?(attrs)
 
       @title = attrs[:title]
       @artist = attrs[:artist]
-      @link = attrs[:link]
+      @url = attrs[:url]
     end
 
     def save(dir = nil)
@@ -46,7 +46,7 @@ module Gmusic
     def attrs_invalid?(attrs)
       return true if attrs.size != 3
       attrs.each_key do |key|
-        return true unless [:title, :artist, :link].include? key
+        return true unless [:title, :artist, :url].include? key
       end
 
       false
