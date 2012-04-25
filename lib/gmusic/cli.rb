aa899c67e9c52ddb09c98c025964f5ee1f358d76
Jeweller-Tsai <jiangnan34@gmail.com>
2012-04-25 16:00:44 +0800

    Added search and download command

--- lib/gmusic/cli.rb
+++ lib/gmusic/cli.rb
@@ -3,20 +3,82 @@ require 'gmusic'
 
 module Gmusic
   class CLI < Thor
-    argument :option
-    desc 'song', 'Search and download songs'
-    method_options %w( title -t ) => :string
-    def song
-      case option
-      when 'search'
-        results = Song.search_by_title(options[:title].gsub(/-/, ' '))
-        reporter = SongReporter.new
-        reporter.decorate results
-      when 'download'
-        puts 'download'
-      else
-        raise 'invalid action'
-      end
+    map 's' => :search, 'd' => :download
+
+    desc 'search', 'Search songs or albums'
+    method_options %w(title -t) => :string, %w(album -a) => :boolean
+    def search
+      title = formatted_title options[:title]
+
+      return search_album(title) if options[:album]
+
+      search_song title
     end
+
+    desc 'download', 'Download songs or albums'
+    method_options %w(title -t) => :string,
+                   %w(album -a) => :string,
+                   %w(directory -d) => :string
+    def download
+      Song.download(formatted_title, options[:directory])
+    end
+
+    private
+
+    def ask_for_a_number(limit)
+      answer = ask "Please type a number in #{(1..limit)}"
+      number = /^\d+$/.match(answer)
+
+      send(__callee__, limit) unless number
+
+      number = number[0].to_i
+      return number if number <= limit
+
+      send(__callee__, limit)
+    end
+
+    def ask_for_numbers(limit)
+      answer = ask <<-EOF
+        Please type one or more numbers in #{(1..limit)}, sperated by comma!
+        Type 0 or any non-digit to to download the whole album.
+      EOF
+      numbers = answer.strip.split(/\s*,\s*/).map(&:to_i)
+
+      numbers.uniq # non-digit char will be 0 when received :to_i
+                   # also restrict duplications
+    end
+
+    def formatted_title(str)
+      str.gsub(/-/, ' ')
+    end
+
+    def search_album(title)
+      reporter = AlbumReporter.decorate do
+        Album.search(title: title)
+      end
+
+      albums = reporter.subject
+      id = ask_for_a_number(albums.size)
+      songs = albums[id].songs
+      reporter.list songs
+
+      ids = ask_for_numbers songs.size
+      #TODO download songs, refactor it later
+      songs = ids.map {|i| songs[i-1] }
+
+      Download::Agent.download(songs)
+    end
+
+    def search_song(title)
+      reporter = SongReporter.decorate do
+        Song.search_by_title(title)
+      end
+
+      songs = reporter.subject
+      id = ask_for_a_number(songs.size) - 1
+
+      songs[id].save
+    end
+
   end
 end
