c30d17f277091fa26c21177d26e3b89fb722e947
Jeweller-Tsai <jiangnan34@gmail.com>
2012-04-25 16:01:36 +0800

    Autoload files

--- lib/gmusic.rb
+++ lib/gmusic.rb
@@ -3,14 +3,21 @@ module Gmusic
   autoload :Artist, 'gmusic/models/artist'
   autoload :Album, 'gmusic/models/album'
   autoload :Link, 'gmusic/models/link'
+  autoload :CLIReporter, 'gmusic/reporters/cli_reporter'
   autoload :SongReporter, 'gmusic/reporters/song_reporter'
+  autoload :AlbumReporter, 'gmusic/reporters/album_reporter'
+  autoload :Download, 'gmusic/download/agent'
+  autoload :AsyncRequest, 'gmusic/http/async_request'
 
   module Search
     autoload :Engine, 'gmusic/search/engine'
+    autoload :Result, 'gmusic/search/result'
 
-    SEARCH_URL = %Q{http://www.google.cn/music/search?q=}
+    SEARCH_URL = %Q(http://www.google.cn/music/search?q=)
+    ALBUM_URL = %Q(http://www.google.cn/music/album?id=)
     DOWNLOAD_URL = %Q(http://www.google.cn/music/top100/musicdownload?id=%s)
     SEARCH_OPTSTIONS = [:title, :artist, :album, :lyric]
+    DEFAULT_DIRECTORY = File.expand_path '~/Downloads'
   end
 
 end
