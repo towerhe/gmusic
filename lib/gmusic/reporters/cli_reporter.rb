13b6e0d5b7fb51be3b2001139425779f628c4d0a
Jeweller-Tsai <jiangnan34@gmail.com>
2012-04-25 16:10:48 +0800

    Extracted shared methods into cli reporter

---
+++ lib/gmusic/reporters/cli_reporter.rb
@@ -0,0 +1,38 @@
+require 'command_line_reporter'
+
+module Gmusic
+  module CLIReporter
+    include CommandLineReporter
+
+    attr_accessor :subject
+
+    def inilitialize(formatter = :progress)
+      self.formatter = formatter.to_s
+    end
+
+    def header(title)
+      super(title: title, color: 'green',
+            align: 'center', rule: true,
+            bold: true)
+    end
+
+    def list(array)
+      table border: true do
+        thead = { id: 10, title: 20, artist: 20, url: 45 }
+
+        row color: 'red', bold: true do
+          thead.each { |k, v| column k, width: v, padding: 2 }
+        end
+
+        array.each_with_index do |item, index|
+          row color: 'green' do
+            column (index + 1).to_s
+            thead.shift
+            thead.each_key { |method| column item.send(method) }
+          end
+        end
+      end
+    end
+
+  end
+end
